ab553fb0f02b930eb0f58338c05ab9cf447a537e
Nikhar Abbas <nikhar.abbas@colorado.edu>
2018-10-24 14:01:21 -0600

    Some Linearization Analysis updates

--- MATLAB_Toolbox/FAST_PostProcess/PlotOutputs.m
+++ MATLAB_Toolbox/FAST_PostProcess/PlotOutputs.m
@@ -4,11 +4,15 @@
 %% Load fAST data
 
 % simulink or fast?
-type = 2;                   % 1 for fast, 2 for simulink
+type = 1;                   % 1 for fast outfile, 2 for simulink 
+% save figures?
+sfig = 0;                   % 1 to save figures
+%save data?
+sdata = 0;                  % 1 to save data
 
 if type == 1
     % Load FAST output file
-    file = ['/Users/nabbas/Documents/TurbineModels/DTU_10MW/DTU_10MW_RWT.out'];
+    file = '/Users/nabbas/Documents/TurbineModels/DTU_10MW/DTU_10MW_RWT.out';
     fastout = LoadFastOut(file);
 elseif type == 2
     fastout = simout;
@@ -18,22 +22,34 @@ end
 
 fastplot = fastout;
 
-
 %% Define desired FAST outputs to plot
 outstr = {'Wind1VelX';
-    'BldPitch1';
+        'BldPitch1';
              'GenTq';
             'RotSpeed';
             'GenPwr';
+            'YawBrTDxt';
 };
         
 %% Plot Outputs
 for j = 1:length(outstr)
-    figure(100+j)
+    fid = figure(100+j);
     plot(fastplot.Time, fastplot.(outstr{j}))
     xlabel('Time (s)')
     title(outstr{j})
     hold on
+    
+    % Save figures
+    runtype = 'VSPI_NoFilt_step_fixed';
+    if sfig == 1
+        root = '/Volumes/GoogleDrive/My Drive/FOWT_ResearchCollab/Figures/GenericControl_v0/';        
+        saveas(fid, [root, outstr{j}, '_', runtype], 'png')
+    end
+    % Save Data
+    if sdata == 1
+        root = '/Volumes/GoogleDrive/My Drive/FOWT_ResearchCollab/simout/GenericControl_v0/';
+        save([root,runtype], 'fastout')
+    end
 end
 
 
